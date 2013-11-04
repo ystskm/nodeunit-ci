379a4d0a27a9000ef556d862f1633106efdedc45
ystskm <brilliantpenguin@gmail.com>
2013-11-04 15:10:04 +0900

    timeout support

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # nodeunit-ci
 
 Support for CI (continuous integration) with nodeuint.
+Timeout can be set when start running.
 
 ## Install
 
@@ -9,11 +10,15 @@ Install with [npm](http://github.com/isaacs/npm):
     npm install nodeunit-ci
 
 ## API - simple test runner
-
+    // Default timeout is 60sec
     require('nodeunit-ci').run('basic', __dirname);
 
-## STDOUT
+### STDOUT
     4 Nov 13:48:51 - - basic.js (7ms, 100% perfect!)
+  
+## API - multiple + timeout(10sec)
+  
+    require('nodeunit-ci').run('basic', __dirname, 10000);
   
 ## see for test fileset
   - [node-localelist](https://github.com/ystskm/node-localelist)  
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var nodeunit = require('nodeunit');
 
 // running target
 var _runner = null;
+var _lastnm = null;
 
 // constant
 var State = {
@@ -15,8 +16,9 @@ var State = {
 }
 
 //constant
-var ErrMsg = {
-  AlreadyInUse: 'This test runner have already called run() method.'
+var DefaultTimeout = 1000 * 60, ErrMsg = {
+  AlreadyInUse: 'This test runner have already called run() method.',
+  Timeout: 'Execution timeout.'
 };
 
 //exports
@@ -25,7 +27,6 @@ function Runner() {
   if(!(this instanceof Runner))
     return new Runner();
   this._state = State.Pending;
-  this.ok = 0, this.err = 0, this.dur = 0;
 }
 
 var RProtos = {
@@ -36,14 +37,15 @@ for( var i in RProtos)
   Runner.prototype[i] = RProtos[i];
 Runner.run = _run;
 
-function run(files, opts) {
+function run(files, opts, timeout) {
 
-  _runner = this;
+  _runner = this, _runner._timer = setTimeout(_timeout,
+    typeof timeout == 'number' ? timeout: DefaultTimeout);
 
   // already in use or used, throws error.
   if(_runner.state() != State.Pending)
     throw new Error(Error.AlreadyInUse + ' :' + _runner.state());
-  _runner.state(State.Running);
+  _runner.state(State.Running), _init();
 
   //{opts}[String || Object]
   //  makePath: a function to get a test suite
@@ -95,26 +97,51 @@ function _makePathFn(fix) {
 /**
  * @ignore
  */
+function _init() {
+  _runner.ok = 0, _runner.err = 0, _runner.dur = 0;
+}
+
+/**
+ * @ignore
+ */
 function _done() {
+  clearTimeout(_runner._timer);
   _runner.err && (function() {
     throw new Error(_runner.err + ' Error(s).');
+  })();
+  _runner = null, _lastnm = null;
+}
+
+/**
+ * @ignore
+ */
+function _timeout() {
+  util.error(ErrMsg.Timeout);
+  var lrun = _runner, lnmsg = 'Last complete: ' + _lastnm;
+  _runner = null, _lastnm = null;
+  lrun.err ? (function() {
+    throw new Error(lnmsg + ', ' + lrun.err + ' Error(s).');
+  })(): (function() {
+    throw new Error(lnmsg);
   })();
 }
 
 nodeunit.on('complete', function(name, results) {
 
-  var p = parseInt(results.passes() / results.length * 1000) / 10;
-  var s = p == 100 ? 'perfect!': 'ok';
-  var m = results.duration + 'ms, ' + p + '% [' + results.passes() + '/'
-    + results.length + '] ' + s;
-  util.log('- ' + name + ' (' + m + ')');
+  var p_cnt = results.passes(), a_cnt = results.length
+  var p = a_cnt ? parseInt(p_cnt / a_cnt * 1000) / 10: 100;
+  var s = p == 100 ? 'perfect!': 'ok', m = results.duration + 'ms, ' + p
+    + '% [' + p_cnt + '/' + a_cnt + '] ' + s;
+  _lastnm = name, util.log('- ' + name + ' (' + m + ')');
 
-  _runner.ok += results.passes(), _runner.dur += results.duration;
+  _runner.ok += a_cnt, _runner.dur += results.duration;
   results.forEach(function(rslt) {
     if(rslt.error == null)
       return;
     _runner.err++, console.log("\t", rslt.method, rslt.message, rslt.error,
       rslt.passed(), rslt.failed());
   });
+
+  _init();
 
 });
